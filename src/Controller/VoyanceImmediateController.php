cfe87a435f44a4b2b3172ab9d7ec2898256e8d4d
auxioma <82117674+Auxioma@users.noreply.github.com>
2023-07-21 18:03:28 +0200

    H mob.

--- src/Controller/VoyanceImmediateController.php
+++ src/Controller/VoyanceImmediateController.php
@@ -4,13 +4,20 @@ namespace App\Controller;
 
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Repository\ModeDeConsultationRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class VoyanceImmediateController extends AbstractController
 {
-    #[Route('/voyance-immediate', name: 'app_forfait')]
-    public function index(): Response
+    public function __construct(
+        private ModeDeConsultationRepository $modeDeConsultationRepository
+    ){}
+
+    #[Route('/voyance-immediate/{Slug}', name: 'app_voyance_immediate')]
+    public function index($Slug): Response
     {
-        return $this->render('voyance_immediate/index.html.twig');
+        return $this->render('voyance_immediate/index.html.twig', [
+            'modeDeConsultation' => $this->modeDeConsultationRepository->findOneBy(['Slug' => $Slug])
+        ]);
     }
 }
--- src/Entity/ModeDeConsultation.php
+++ src/Entity/ModeDeConsultation.php
@@ -2,12 +2,22 @@
 
 namespace App\Entity;
 
-use App\Repository\ModeDeConsultationRepository;
+use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
+use App\Entity\Traits\CreatedAtTrait;
+use App\Entity\Traits\UpdatedAtTrait;
+use Symfony\Component\HttpFoundation\File\File;
+use App\Repository\ModeDeConsultationRepository;
+use Vich\UploaderBundle\Mapping\Annotation as Vich;
 
 #[ORM\Entity(repositoryClass: ModeDeConsultationRepository::class)]
+#[Vich\Uploadable]
 class ModeDeConsultation
 {
+
+    use CreatedAtTrait;
+    use UpdatedAtTrait;
+
     #[ORM\Id]
     #[ORM\GeneratedValue]
     #[ORM\Column]
@@ -21,6 +31,15 @@ class ModeDeConsultation
 
     #[ORM\Column(length: 255)]
     private ?string $Slug = null;
+
+    #[ORM\Column(type: Types::TEXT)]
+    private ?string $GrandeDescription = null;
+
+    #[Vich\UploadableField(mapping: 'service', fileNameProperty: 'imageName')]
+    private ?File $imageFile = null;
+    
+    #[ORM\Column(nullable: true)]
+    private ?string $imageName = null;
 
     public function getId(): ?int
     {
@@ -62,4 +81,52 @@ class ModeDeConsultation
 
         return $this;
     }
+
+    public function getGrandeDescription(): ?string
+    {
+        return $this->GrandeDescription;
+    }
+
+    public function setGrandeDescription(string $GrandeDescription): static
+    {
+        $this->GrandeDescription = $GrandeDescription;
+
+        return $this;
+    }
+
+    /**
+     * If manually uploading a file (i.e. not using Symfony Form) ensure an instance
+     * of 'UploadedFile' is injected into this setter to trigger the update. If this
+     * bundle's configuration parameter 'inject_on_load' is set to 'true' this setter
+     * must be able to accept an instance of 'File' as the bundle will inject one here
+     * during Doctrine hydration.
+     *
+     * @param File|\Symfony\Component\HttpFoundation\File\UploadedFile|null $imageFile
+     */
+    public function setImageFile(?File $imageFile = null): void
+    {
+        $this->imageFile = $imageFile;
+
+        if (null !== $imageFile) {
+            // It is required that at least one field changes if you are using doctrine
+            // otherwise the event listeners won't be called and the file is lost
+            $this->UpdatedAt = new \DateTimeImmutable();
+        }
+    }
+
+    public function getImageFile(): ?File
+    {
+        return $this->imageFile;
+    }
+
+    public function setImageName(?string $imageName): void
+    {
+        $this->imageName = $imageName;
+    }
+
+    public function getImageName(): ?string
+    {
+        return $this->imageName;
+    }
+
 }
