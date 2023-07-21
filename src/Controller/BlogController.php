361d3a312a9d7f404b379ff52ea4c3cdf495614f
auxioma <82117674+Auxioma@users.noreply.github.com>
2023-07-21 09:05:28 +0200

    MAJ

--- src/Controller/BlogController.php
+++ src/Controller/BlogController.php
@@ -2,17 +2,40 @@
 
 namespace App\Controller;
 
+use App\Repository\ArticleBlogRepository;
+use App\Repository\CategoryBlogRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class BlogController extends AbstractController
 {
-    #[Route('/blog', name: 'app_blog')]
-    public function index(): Response
+
+    #[Route('/blog/{Slug}/{id}', name: 'app_blog_show_article')]
+    public function ShowArticle(ArticleBlogRepository $article, CategoryBlogRepository $categorie, $Slug, $id): Response
     {
-        return $this->render('blog/index.html.twig', [
-            'controller_name' => 'BlogController',
+        return $this->render('blog/article.html.twig', [
+            'article' => $article->findOneBy(['Slug' => $id]),
+            'categories' => $categorie->findBy(['IsVerified' => true], ['id' => 'ASC'])
         ]);
     }
+
+    #[Route('/blog/{Slug}', name: 'app_blog_show')]
+    public function Show(ArticleBlogRepository $article, CategoryBlogRepository $categorie, $Slug): Response
+    {
+        return $this->render('blog/index.html.twig', [
+            'articles' => $article->findBy(['Categorie' => $categorie->findOneBy(['Slug' => $Slug])]),
+            'categories' => $categorie->findBy(['IsVerified' => true], ['id' => 'ASC'])
+        ]);
+    }
+
+    #[Route('/blog', name: 'app_blog')]
+    public function index(ArticleBlogRepository $article, CategoryBlogRepository $categorie): Response
+    {
+        return $this->render('blog/index.html.twig', [
+            'articles' => $article->findAll(),
+            'categories' => $categorie->findBy(['IsVerified' => true], ['id' => 'ASC'])
+        ]);
+    }
+    
 }
--- src/Entity/ArticleBlog.php
+++ src/Entity/ArticleBlog.php
@@ -7,8 +7,11 @@ use App\Entity\Traits\UpdatedAtTrait;
 use App\Repository\ArticleBlogRepository;
 use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\HttpFoundation\File\File;
+use Vich\UploaderBundle\Mapping\Annotation as Vich;
 
 #[ORM\Entity(repositoryClass: ArticleBlogRepository::class)]
+#[Vich\Uploadable]
 class ArticleBlog
 {
     use CreatedAtTrait;
@@ -34,12 +37,21 @@ class ArticleBlog
     #[ORM\Column]
     private ?bool $IsActive = null;
 
+    #[ORM\Column(type: Types::TEXT)]
+    private ?string $LongDescription = null;
+
+    #[Vich\UploadableField(mapping: 'slider', fileNameProperty: 'imageName')]
+    private ?File $imageFile = null;
+    
+    #[ORM\Column(nullable: true)]
+    private ?string $imageName = null;
+
     public function getId(): ?int
     {
         return $this->id;
     }
 
-    public function getArticle(): ?string
+    public function getArticle(): ?string 
     {
         return $this->Article;
     }
@@ -97,5 +109,52 @@ class ArticleBlog
         $this->IsActive = $IsActive;
 
         return $this;
+    }
+
+    public function getLongDescription(): ?string
+    {
+        return $this->LongDescription;
+    }
+
+    public function setLongDescription(string $LongDescription): static
+    {
+        $this->LongDescription = $LongDescription;
+
+        return $this;
+    }
+
+    /**
+     * If manually uploading a file (i.e. not using Symfony Form) ensure an instance
+     * of 'UploadedFile' is injected into this setter to trigger the update. If this
+     * bundle's configuration parameter 'inject_on_load' is set to 'true' this setter
+     * must be able to accept an instance of 'File' as the bundle will inject one here
+     * during Doctrine hydration.
+     *
+     * @param File|\Symfony\Component\HttpFoundation\File\UploadedFile|null $imageFile
+     */
+    public function setImageFile(?File $imageFile = null): void
+    {
+        $this->imageFile = $imageFile;
+
+        if (null !== $imageFile) {
+            // It is required that at least one field changes if you are using doctrine
+            // otherwise the event listeners won't be called and the file is lost
+            $this->UpdatedAt = new \DateTimeImmutable();
+        }
+    }
+
+    public function getImageFile(): ?File
+    {
+        return $this->imageFile;
+    }
+
+    public function setImageName(?string $imageName): void
+    {
+        $this->imageName = $imageName;
+    }
+
+    public function getImageName(): ?string
+    {
+        return $this->imageName;
     }
 }
