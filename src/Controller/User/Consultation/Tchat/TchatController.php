8d16b5ba0063f95d1d9c97b972de9998d773fc16
auxioma <82117674+Auxioma@users.noreply.github.com>
2023-07-24 17:34:37 +0200

    facture Email

--- src/Controller/User/Consultation/Tchat/TchatController.php
+++ src/Controller/User/Consultation/Tchat/TchatController.php
@@ -2,18 +2,15 @@
 
 namespace App\Controller\User\Consultation\Tchat;
 
-use DateTimeImmutable;
 use Lcobucci\JWT\Signer\Key;
 use Lcobucci\JWT\Token\Builder;
 use Lcobucci\JWT\Signer\Hmac\Sha256;
-use Lcobucci\JWT\Signer\Key\InMemory;
 use Lcobucci\JWT\Encoding\JoseEncoder;
 use Lcobucci\JWT\Encoding\ChainedFormatter;
 use Symfony\Component\HttpFoundation\Cookie;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Lcobucci\JWT\Configuration;
 
 class TchatController extends AbstractController
 {
@@ -21,26 +18,17 @@ class TchatController extends AbstractController
     public function index(): Response
     {
         $username = $this->getUser()->getProfile()->getPseudo();
-        /*
+
         $token = (new Builder(new JoseEncoder(), new ChainedFormatter()))
-            ->withClaim('mercure', ['subscribe' => [sprintf("/%s", $username)]])
+            ->withClaim('mercure', [
+                                        'subscribe' => [
+                                            sprintf("/%s", $username)
+                                        ]
+                                    ])
             ->getToken(
                 new Sha256(),
                 new Key($this->getParameter('mercure_secret_key'))
             )
-        ;
-        */
-
-  
-        // Creer moi le token avec jwt 5
-        $configuration = Configuration::forSymmetricSigner(
-            new Sha256(),
-            InMemory::plainText($this->getParameter('mercure_secret_key'))
-        );	
-
-        $token = $configuration->builder()
-            ->withClaim('mercure', ['subscribe' => [sprintf("/%s", $username)]])
-            ->getToken($configuration->signer(), $configuration->signingKey())
         ;
         
         $response =  $this->render('user/consultation_tchat/index.html.twig');
