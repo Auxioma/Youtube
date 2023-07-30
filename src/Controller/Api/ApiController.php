722cf117eae7bc9f47678c9325ca3eb188650d07
Auxioma <auxioma@yandex.fr>
2023-07-30 17:59:23 +0200

    AJAX coté admin - les status des modes de voyance

--- src/Controller/Api/ApiController.php
+++ src/Controller/Api/ApiController.php
@@ -2,8 +2,11 @@
 
 namespace App\Controller\Api;
 
-use Symfony\Component\Routing\Annotation\Route;
+use App\Entity\ModeDeConsultation;
 
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
 use App\Repository\ModeDeConsultationRepository;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\StreamedResponse;
@@ -30,5 +33,27 @@ class ApiController extends AbstractController
             ];
         }
         return $this->json($data);
+    }
+
+    #[Route('/api/bouton/update/settings', name: 'update_settings', methods: ['POST'])]
+    public function updateSettings(Request $request, EntityManagerInterface $entityManager)
+    {
+        // Récupérer les données du formulaire envoyées via AJAX
+        $isTelephoneActive = $request->request->get('telephone');
+        $isEmailActive = $request->request->get('email');
+        $isTchatActive = $request->request->get('tchat');
+
+        // Effectuer la mise à jour en base de données en fonction des données reçues
+        // Mettez ici votre propre logique pour mettre à jour les données en base de données.
+        // Voici un exemple d'utilisation d'une entité Doctrine :
+
+        $settings = $entityManager->getRepository(ModeDeConsultation::class)->findAll();
+        $settings[0]->setIsOnline($isEmailActive);
+        $settings[1]->setIsOnline($isTelephoneActive);
+        $settings[2]->setIsOnline($isTchatActive);
+        $entityManager->flush();
+
+        // Répondre avec un message de succès ou d'erreur
+        return new JsonResponse(['success' => true]);
     }
 }
