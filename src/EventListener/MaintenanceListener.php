890f4175e6d700c04a789c89b4e8b8bdd406ee7b
auxioma <auxioma@yandex.fr>
2023-08-12 13:14:37 +0200

    vérif IP

--- src/EventListener/MaintenanceListener.php
+++ src/EventListener/MaintenanceListener.php
@@ -16,9 +16,9 @@ class MaintenanceListener{
     }
 
     public function onKernelRequest(RequestEvent $event){
-
+        dd($event->getRequest()->getClientIp());
         // On verifie l'IP 127.0.0.1 ou 81.67.188.146 pour ne pas afficher la page de maintenance
-        if( $event->getRequest()->getClientIp() === '127.0.0.1' || 
+        if( 
             $event->getRequest()->getClientIp() === '81.67.188.146' || 
             $event->getRequest()->getClientIp() === '91.166.94.206'){
             return;
