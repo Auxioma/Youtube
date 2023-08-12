191ebd91fea3df07e9bd060d035fbff55ce522f1
auxioma <auxioma@yandex.fr>
2023-08-12 13:19:30 +0200

    Adaptation IP

--- src/EventListener/MaintenanceListener.php
+++ src/EventListener/MaintenanceListener.php
@@ -16,11 +16,11 @@ class MaintenanceListener{
     }
 
     public function onKernelRequest(RequestEvent $event){
-        dd($event->getRequest()->getClientIp());
         // On verifie l'IP 127.0.0.1 ou 81.67.188.146 pour ne pas afficher la page de maintenance
         if( 
-            $event->getRequest()->getClientIp() === '81.67.188.146' || 
-            $event->getRequest()->getClientIp() === '91.166.94.206'){
+            $event->getRequest()->getClientIp() === '81.67.188.146' or
+            $event->getRequest()->getClientIp() === '91.166.94.206' or
+            $event->getRequest()->getClientIp() === '2a01:e0a:197:b3a0:8153:9fc8:11f6:fa5a'){
             return;
         }
 
