7b64b5953b8a3fb790b7d072d07485a30955ad93
auxioma <auxioma@yandex.fr>
2023-09-04 09:03:35 +0200

    Moyenne des avis Home

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Repository\ArticleBlogRepository;
+use App\Repository\AvisRepository;
 use App\Repository\ModeDeConsultationRepository;
 use App\Repository\SliderRepository;
 use Symfony\Component\HttpFoundation\Response;
@@ -14,18 +15,40 @@ class MainController extends AbstractController
     public function __construct(
         private SliderRepository $SliderRepository,
         private ModeDeConsultationRepository $ModeDeConsultationRepository,
-        private ArticleBlogRepository $ArticleBlog
+        private ArticleBlogRepository $ArticleBlog,
+        private AvisRepository $AvisRepository
     )
     {}
 
     #[Route('/', name: 'app_main')]
     public function index(): Response
     {
+        $avis = $this->AvisRepository->findAll();
+        $moyenne = 0;
+        foreach ($avis as $item) {
+            $moyenne += $item->getNote();
+        }
+
+        $moyenne = $moyenne / count($avis);
+
+        /**
+         * je met dans un [] le % des avis Tres satisfait 3 à 5
+         * satisfait 2 à 3
+         * pas satisfait 1 à 2
+         */
+        $moyenneAvis = [
+            'tresSatisfait' => round(count($this->AvisRepository->findBy(['Note' => 5, 'Note' => 4])) / count($avis) * 100),
+            'pasSatisfait' => round(count($this->AvisRepository->findBy(['Note' => 3, 'Note' => 2])) / count($avis) * 100),
+            'pasDuToutSatisfait' => round(count($this->AvisRepository->findBy(['Note' => 1])) / count($avis) * 100),
+        ];
+
         return $this->render('main/HomePage.html.twig', [
             'sliders' => $this->SliderRepository->findBy(['IsActive' => true]),
             'modeDeConsultations' => $this->ModeDeConsultationRepository->findAll(),
             'horoscopes' => ['belier', 'taureau','gemeaux', 'cancer', 'lion', 'vierge', 'balance','scorpion', 'sagittaire','capricorne',  'verseau', 'poissons'],
-            'articleBlogs' => $this->ArticleBlog->findBy(['IsActive' => true], ['id' => 'DESC'], 3)
+            'articleBlogs' => $this->ArticleBlog->findBy(['IsActive' => true], ['id' => 'DESC'], 3),
+            'moyenne' => $moyenne,
+            'moyenneAvis' => $moyenneAvis
         ]);
     }
 }
